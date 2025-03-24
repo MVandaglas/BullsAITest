509b4f71181cc9bc4e02b0cb835387b03c8d3e13
MVandaglas <martijn.wissink@vandaglas.com>
2025-03-24 13:23:08 +0100

    Update Synonyms.py

--- Synonyms.py
+++ Synonyms.py
@@ -102,7 +102,7 @@ synonym_dict = {
     "10-8 HR++": "1006450",
     "4-15-4": "1006350",
     "4-16-4": "1006350",
-    "Pyroguard impact 7mm": "1006450",
+    "Pyroguard impact 7mm": "1007627",
     "IP ThermControl 33/1 - 33/1LE1.1": "1006351",
     "IP ThermControl 04 - 04LE1.1": "1006350",
     "6-15A-4": "1006353",
