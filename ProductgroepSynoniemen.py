754aa6ca098a30049477d9478cd5b5b10f583c5f
MVandaglas <martijn.wissink@vandaglas.com>
2025-03-31 15:10:53 +0200

    Update ProductgroepSynoniemen.py

--- ProductgroepSynoniemen.py
+++ ProductgroepSynoniemen.py
@@ -2,10 +2,10 @@
 
 Productgroepen_dict = { 
   "Alfa": ["alfa", "hr++", "xn"],
-  "SS Zero": ["zero", "silver star zero", "silver star", "1.0", "silverstar", "silverstar zero"],
+  "SS Zero": ["ss zero", "sszero", "ss", "zero", "silver star zero", "silver star", "1.0", "silverstar", "silverstar zero"],
   "Eclaz One": ["eclaz", "80/60", "eclazone", "one 80/60", "eclaz-one"],
-  "Energy": ["sun", "72/38"],
-  "XTREME61/29": ["61/29", "xtreme 61/29", "xtreme", "xtreme", "xtreme 61/29", "xtreme61/29", "xtr", "xtr61/29"],
+  "Energy": ["sun", "72/38", "energy"],
+  "XTREME61/29": ["61/29", "xtreme 61/29", "xtreme", "xtreme", "xtreme 61/29", "xtreme61/29", "xtr", "xtr61/29", "xtreme61/29"],
   "SKN154": ["skn 154", "52/38", "skn154"],
   "SKN165": ["skn 165", "skn 165b", "skn165b", "skn 165b", "skn165b", "61/34", "skn165"],
   "Triple": ["triple", "74/54", "hr+++", "driedubbel"]
