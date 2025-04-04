80143b80f1bc74037c93445839c59aac563d60e8
MVandaglas <martijn.wissink@vandaglas.com>
2025-04-04 10:54:03 +0200

    Update ProductgroepSynoniemen.py

--- ProductgroepSynoniemen.py
+++ ProductgroepSynoniemen.py
@@ -3,10 +3,10 @@
 Productgroepen_dict = { 
   "Alfa": ["alfa", "hr++", "xn"],
   "SS Zero": ["ss zero", "sszero", "ss", "zero", "silver star zero", "silver star", "1.0", "silverstar", "silverstar zero"],
-  "Eclaz One": ["eclaz", "80/60", "eclazone", "one 80/60", "eclaz-one", "eclaz one"],
+  "Eclaz One": ["eclaz", "80/60", "eclazone", "one 80/60", "eclaz-one", "eclaz one", "one"],
   "Energy": ["sun", "72/38", "energy"],
-  "XTREME61/29": ["61/29", "xtreme 61/29", "xtreme", "xtreme", "xtreme 61/29", "xtreme61/29", "xtr", "xtr61/29", "xtreme61/29"],
-  "SKN154": ["skn 154", "52/38", "skn154"],
-  "SKN165": ["skn 165", "skn 165b", "skn165b", "skn 165b", "skn165b", "61/34", "skn165"],
+  "XTREME61/29": ["61/29", "xtreme 61/29", "xtreme", "xtr 61/29", "xtreme61/29", "xtr", "xtr61/29", "xtreme61/29"],
+  "SKN154": ["skn 154", "52/38", "skn154", "skn52/38", "skn 52/38"],
+  "SKN165": ["skn 165", "skn 165b", "skn165b", "skn 165b", "skn165b", "61/34", "skn165", "skn 61/34"],
   "Triple": ["triple", "74/54", "hr+++", "driedubbel"]
 }
